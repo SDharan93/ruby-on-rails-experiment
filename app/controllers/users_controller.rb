f090329e3d40bee9ba685c62d7e647fa75bb2183
Shane Dharan <shanedharan@gmail.com>
2018-06-09 16:18:22 -0400

    Fix lint errors with the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
+# The User's controller is responsible for routing user specific requests.
+# Currently this controller is a stub.
 class UsersController < ApplicationController
-  
-  def new
-  end
+  def new(); end
 end
