4839510642221b79bf3d0a7110714d25abbfacba
Shane Dharan <shanedharan@gmail.com>
2018-06-09 15:51:41 -0400

    Add user email validation tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,4 +39,22 @@ class UserTest < ActiveSupport::TestCase
 		@user.email = 'a' * 100
 		assert(@user.valid?)
 	end
+
+	test 'email should accept valid email addresses' do
+		valid_emails = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+        valid_emails.each do |valid_email|
+        	@user.email = valid_email
+        	assert(@user.valid?, "#{valid_email} should be valid")
+        end
+	end
+
+	test 'email should reject invalid email addresses' do
+		invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+        invalid_addresses.each do |invalid_email|
+        	@user.email = invalid_email
+        	assert_not(@user.valid?, "#{invalid_email} should not be valid")
+        end
+	end	
 end
