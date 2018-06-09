2fd9bf88a8eb5b2f529a00593f1d1636267c2e59
Shane Dharan <shanedharan@gmail.com>
2018-06-09 16:13:46 -0400

    Fix lint errors in static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,11 @@
+# Controller for static pages. This controller does not have much logic
+# as it only renders the static pages.
 class StaticPagesController < ApplicationController
-  def home
-  end
+  def home(); end
 
-  def help
-  end
+  def help(); end
 
-  def about
-  end
-  
-  def contact
-  end
-  
+  def about(); end
+
+  def contact(); end
 end
