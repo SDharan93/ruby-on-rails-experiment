e011888fcf93513eb21b5fd97fff54874cb669f3
Shane Dharan <shanedharan@gmail.com>
2018-06-10 22:24:29 -0400

    Add tests to make sure emails are saved in lowercase

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -73,4 +73,11 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not(duplicate_user.valid?)
   end
+
+  test 'should save emails as lower-case' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal(mixed_case_email.downcase, @user.reload.email)
+  end
 end
