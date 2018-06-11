3b9f20b3fe088b011edf5850c6b25d7e8ac78bf9
Shane Dharan <shanedharan@gmail.com>
2018-06-10 22:25:01 -0400

    Add before save callback to make emails lowercase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 # Model representing the User Table.
 class User < ApplicationRecord
+  before_save(:normalize_attributes)
+
   validates(:name, { presence: true, length: { maximum: 50 } })
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:email, { presence: true, length: { maximum: 100 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false } })
+
+  private
+
+  def normalize_attributes
+    self.email = self.email.downcase
+  end
 end
